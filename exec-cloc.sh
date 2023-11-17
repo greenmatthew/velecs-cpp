dc15f2be8d697f0987f8ab9f76ad1f5ab4b531bf
Matthew Green <greenmatthew07@gmail.com>
2023-11-17 12:30:56 -0600

    Added a shell script that uses the CLI tool clcloc to count the lines of code I have written to feed my curiosity.

---
+++ exec-cloc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run cloc with forced language options for .DISABLED files
+cloc --ignored=ignored.txt --force-lang="C++",cpp.DISABLED --force-lang="C/C++ Header",h.DISABLED harvest_havoc/ velecs/ libs/CMakeLists.txt CMakeLists.txt README.md
+
+# Check if ignored.txt was created and is not empty
+if [ -s ignored.txt ]; then
+    cat ignored.txt
+    rm ignored.txt
+fi
