facd70b909d3c5f8d66a61d0fd215feee41522c2
Matthew Green <greenmatthew07@gmail.com>
2023-11-30 14:07:16 -0600

    Small update to cloc script.

--- exec-cloc.sh
+++ exec-cloc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Run cloc with forced language options for .DISABLED files
-cloc --ignored=ignored.txt --force-lang="C++",cpp.DISABLED --force-lang="C/C++ Header",h.DISABLED harvest_havoc/ velecs/ libs/CMakeLists.txt CMakeLists.txt README.md
+cloc \
+--ignored=ignored.txt \
+--force-lang="C++",cpp.DISABLED \
+--force-lang="C/C++ Header",h.DISABLED \
+harvest_havoc/ \
+velecs/ \
+libs/CMakeLists.txt \
+CMakeLists.txt \
+*.md
 
 # Check if ignored.txt was created and is not empty
 if [ -s ignored.txt ]; then
